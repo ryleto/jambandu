fc6b73a36139905c0423161f06e049110d533670
Ryan Leto <ryjleto@gmail.com>
2016-01-25 01:38:23 +0000

    updates

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.2.3'
 gem 'rails', '4.2.5'
 gem 'pg'
+gem 'bootstrap-sass'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -9,9 +10,13 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'puma', '~> 2.15', '>= 2.15.3'
+gem 'devise'
+gem 'pundit'
+gem 'acts_as_commentable'
+
 # helpers
 gem 'active_link_to', '~> 1.0.3'
-gem 'acts_as_commentable'
+
 group :development, :test do
   gem 'byebug'
 end
@@ -19,9 +24,6 @@ group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
 end
-gem 'bootstrap-sass'
-gem 'devise'
-gem 'pundit'
 group :development do
   gem 'better_errors'
   gem 'quiet_assets'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   after_action :verify_authorized
+  #after_action :send_welcome_email, only: [:create]
 
   def index
     @users = User.all
@@ -21,6 +22,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     authorize @user
     if @user.save
+        send_welcome_message(self)
         redirect_to @user, notice: "User succesfully created!" 
     else
         redirect_to users_path, :alert => "Unable to add new user."
@@ -58,6 +60,10 @@ class UsersController < ApplicationController
     
     def user_params
       params.require(:user).permit(:role, :username, :name, :email, :password, :password_confirmation, :current_password, :company_id)
+    end
+    
+    def send_welcome_message(user)
+      UserTransactionMailer.welcome_message(user).deliver_now
     end
 
 end
