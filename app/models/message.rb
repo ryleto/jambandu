8acea611528dbb6d61c3a63e27b730a090f89a42
Ryan Leto <ryjleto@gmail.com>
2016-02-08 06:18:56 +0000

    updates

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
     include ActiveModel::Model
     attr_accessor :name, :company, :email, :content
-    validates: :name, :email, :content, :presence => true
+    validates :name, :email, :content, :presence => true
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
