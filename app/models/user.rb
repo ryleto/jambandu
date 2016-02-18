afa03be992c77f07cfcc7ad634e22fdd575ecb37
Ryan Leto <ryjleto@gmail.com>
2016-02-18 00:20:27 +0000

    adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ActiveRecord::Base
   after_initialize :set_default_role, :if => :new_record?
   
   before_save   :downcase_email
-  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
