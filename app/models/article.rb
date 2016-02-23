b1a95b7ccac75996546c17e8f4ddd23fee99d004
Ryan Leto <ryjleto@gmail.com>
2016-02-23 04:24:15 +0000

    validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ActiveRecord::Base
     default_scope -> { order(pubdate: :desc) }
     acts_as_commentable
     acts_as_taggable
+    validates :pubdate, :title, :source, :presence => true
+    validates :title, :source, :uniqueness => true
+    validates :source, :format => URI::regexp(%w(http https))
 end
