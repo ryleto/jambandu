104160a6d4bb1570485dfb3f813a8de400f8718a
Ryan Leto <ryjleto@gmail.com>
2016-03-24 14:43:19 +0000

    removing model

--- app/assets/javascripts/articles.coffee
+++
@@ -1,3 +0,0 @@
-$(document).ready ->
-  $('.datepicker').datepicker({format: 'yyyy-mm-dd', 'autoclose': true})
-  return
--- app/models/article.rb
+++
@@ -1,9 +0,0 @@
-class Article < ActiveRecord::Base
-    has_many :comments
-    default_scope -> { order(pubdate: :desc) }
-    acts_as_commentable
-    acts_as_taggable
-    validates :pubdate, :title, :source, :presence => true
-    validates :title, :source, :uniqueness => true
-    validates :source, :format => URI::regexp(%w(http https))
-end
