81ccd1f3f9d5bcbbc9ad960a66d96e05a1e7ba58
Ryan Leto <ryjleto@gmail.com>
2017-02-20 20:36:11 +0000

    route updates

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   end
   
   root to: 'static_pages#index'
-  get   'about'     => 'static_pages#about'
-  get   'products'  => 'static_pages#products'
-  get   'dashboard' => 'static_pages#home'
-  get   'error_redirect' => 'errors#redirect'
-  get   'contact'   => 'messages#new', as: 'contact'
-  post  'contact'   => 'messages#create'
-  get   'tags/:tag' => 'articles#index', as: :tag
+  get   'about', to: 'static_pages#about'
+  get   'products', to: 'static_pages#products'
+  get   'dashboard', to: 'static_pages#home'
+  get   'error_redirect' , to: 'errors#redirect'
+  get   'contact', to: 'messages#new', as: 'contact'
+  post  'contact', to: 'messages#create'
+  get   'tags/:tag', to: 'articles#index', as: :tag
   
   resources :companies
   resources :comments
